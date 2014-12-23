c8b463c9054272d19e6c8d7da177a35acf6953e9
setzu-mattia <ma.setzu2@studenti.unica.it>
2014-12-23 11:06:18 +0100

    Added types and expressions

---
+++ project.ml
@@ -0,0 +1,38 @@
+type ide = string;;
+  
+type loc = int;;
+
+type exp = 
+        Eint of int 
+      | Ebool of bool 
+      | Echar of char
+      | Empty
+      | Cons of exp * exp
+      | Den of ide
+      | Prod of exp * exp
+      | Sum of exp * exp
+      | Diff of exp * exp
+      | Mod of exp * exp
+      | Div of exp * exp
+      | Lessint of exp * exp
+      | Eqint of exp * exp
+      | Iszero of exp
+      | Lesschar of exp * exp
+      | Eqchar of exp * exp
+      | Or of exp * exp
+      | And of exp * exp
+      | Not of exp
+      | Ifthenelse of exp * exp * exp
+      | Let of (ide * exp) list * exp      
+      | Fun of ide list * exp
+      | Apply of exp * exp list;;
+
+type generic = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z;;
+  
+type typ = 
+    Int 
+  | Bool
+  | Char
+  | List of typ
+  | Fun of typ * typ
+  | Gen of generic;;
